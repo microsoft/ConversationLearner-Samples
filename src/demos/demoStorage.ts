360d5db19e2630db2277da28c437b29426d366dd
Matt Mazzola <mattm@microsoft.com>
2018-03-20 09:24:38 -0700

    Add luis keys to config, update config types to include possible undefined, add guards for redis values (#99)
    * Add luis keys to config, update config types to include possible undefined, add guards for redis values
    
    * Update blis-sdk@0.165.0
    
    * Add Fiddler launch config for testing

--- src/demos/demoStorage.ts
+++ src/demos/demoStorage.ts
@@ -41,6 +41,14 @@ server.post('/api/messages', connector.listen() as any);
 // Stores bot state in a redis cache.  
 // With this option, the bot can be stopped and re-started without losing the state of in-progress sessions.
 // Requires env variables BLIS_REDIS_SERVER and BLIS_REDIS_KEY are set.  You can set these in ../.env.
+
+if (typeof config.redisServer !== 'string' || config.redisServer.length === 0) {
+    throw new Error(`When using Redis storage: redisServer value must be non-empty. You passed: ${config.redisServer}`)
+}
+if (typeof config.redisKey !== 'string' || config.redisKey.length === 0) {
+    throw new Error(`When using Redis storage: redisKey value must be non-empty. You passed: ${config.redisKey}`)
+}
+
 let redisStorage = new RedisStorage({ server: config.redisServer, key: config.redisKey });
 Blis.Init(blisConfig, redisStorage);
 
