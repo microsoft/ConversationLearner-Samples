9bdfd473c4dace1976768a72502663a30782767a
Lars Liden <larsliden@gmail.com>
2018-04-05 09:19:08 -0700

    Memmanager objects (#124)
    * Comments for memory manager using objects
    
    * sdk -> 0.187.0

--- src/app.ts
+++ src/app.ts
@@ -50,11 +50,13 @@ Blis.EntityDetectionCallback(async (text: string, memoryManager: ClientMemoryMan
     memoryManager.EntityValueAsync(entityName: string): Promise<string>;
     memoryManager.EntityValueAsPrebuiltAsync(entityName: string): Promise<MemoryValue[]>;
     memoryManager.EntityValueAsListAsync(entityName: string): Promise<string[]>;
+    memoryManager.EntityValueAsObject<T>(entityName: string): Promise<T | null> {
 
     // Values in memory before new Entity detection
     memoryManager.PrevEntityValue(entityName: string): (string | null)
     memoryManager.PrevEntityValueAsPrebuilt(entityName: string): MemoryValue[]
     memoryManager.PrevEntityValueAsList(entityName: string): string[]
+    memoryManager.PrevValueAsObject<T>(entityName: string): (T | null)
 
     // Memory manipulation methods
     memoryManager.RememberEntityAsync(entityName: string, entityValue: string): Promise<void>;
