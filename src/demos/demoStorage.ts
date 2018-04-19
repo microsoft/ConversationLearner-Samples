e65481aa0cdfbf03f42ac86494a2492d5bb710b6
Lars Liden <larsliden@gmail.com>
2018-04-19 08:18:53 -0700

    Add MIT License (#147)

--- src/demos/demoStorage.ts
+++ src/demos/demoStorage.ts
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.  
+ * Licensed under the MIT License.
+ */
 import * as path from 'path'
 import * as restify from 'restify'
 import * as BB from 'botbuilder'
