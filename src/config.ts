363f88673b5fce5ba600707ef3fd51ef5fc6d7fc
Lars Liden <larsliden@gmail.com>
2018-04-19 08:14:13 -0700

    Add MIT License

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.  
+ * Licensed under the MIT License.
+ */
 import * as dotenv from 'dotenv'
 import * as convict from 'convict'
 import { ICLOptions } from 'conversationlearner-sdk';
