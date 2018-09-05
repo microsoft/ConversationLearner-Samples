74691af44232507044a89e076ca1000ffd013e11
Shahin Shayandeh <shahins@microsoft.com>
2018-09-05 16:09:08 -0700

    Add entityName and type to labeled entities

--- src/history.ts
+++ src/history.ts
@@ -19,6 +19,13 @@ async function history(appId : string) {
         console.log(`History for ${Math.round(100*(++count/source.trainDialogs.length))}% of train dialogs have been retrieved!`)
     }
 
+    
+    let entityDict : Map<string, models.EntityBase> = new Map()
+    source.entities.forEach(entity => {
+        entityDict.set(entity.entityId, entity)
+    })
+    
+
     let idx = 0;
     let compactHistory : Array<string> = []
     source.trainDialogs = source.trainDialogs.map(trainDialog => {
@@ -28,7 +35,42 @@ async function history(appId : string) {
         trainDialog.rounds = trainDialog.rounds.map(round => {
             round = <any>{userInput: activities[index++], ...round};
             compactHistory.push(`+${(round as any).userInput.text}`)
-            round.scorerSteps = round.scorerSteps.map(step => <any>{...step, output: activities[index++]})
+            
+            round.scorerSteps = round.scorerSteps.map(step => {
+                step.input.filledEntities = step.input.filledEntities.map(entity => {
+                    let entityDefintion = entityDict.get(entity.entityId as string)
+                        if(entityDefintion !== undefined)
+                        {
+                            return {
+                                ...entity, 
+                                entityType: entityDefintion.entityType, 
+                                entityName : entityDefintion.entityName
+                            }
+                        }
+                    return entity
+                })
+                let newStep = <any>{...step, output: activities[index++]}
+                return newStep
+            } )
+            
+            // add entity information to labeled entities
+            round.extractorStep.textVariations = round.extractorStep.textVariations.map(textVariation => {
+                textVariation.labelEntities = textVariation.labelEntities.map(label => { 
+                    let entityDefintion = entityDict.get(label.entityId)
+                    if(entityDefintion !== undefined)
+                    {
+                        return {
+                            ...label, 
+                            entityType: entityDefintion.entityType, 
+                            entityName : entityDefintion.entityName
+                        }
+
+                    }
+                    return label
+                }
+                )   
+                return textVariation
+            })
             compactHistory = compactHistory.concat(round.scorerSteps.map(step => `-${(step as any).output.text}`))
             return round; 
         })
@@ -39,7 +81,7 @@ async function history(appId : string) {
     await writeFile('history.json', JSON.stringify(source))
     let s = ""
     compactHistory.map(value => s += `${value}\n`)
-    await writeFile('history.compact.json', s)
+    await writeFile('history.compact.txt', s)
 }
 
 history('d14dc392-7b4d-4e7a-8911-2cf672e18ce3').then(() => console.log('history have been retrieved! Check history.json.'), (reason) => console.log(reason));
